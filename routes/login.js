ca4964ebb9943e7e7bc38ee09159126dbf57b2d2
Saleban Abdillahi <01saleban@gmail.com>
2022-02-14 03:41:20 +0000

    login/registration routes

---
+++ routes/login.js
@@ -0,0 +1,32 @@
+/*
+ * All routes for Login are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const app = express()
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+
+    const templateVars = {
+        userId: null
+    }
+
+    if (templateVars.userId) {
+        res.redirect('../')
+    }
+
+    res.render('../views/login.ejs', templateVars)
+  });
+  router.post("/push", (req, res) => {
+
+
+    // This is where we need to set a cookie
+
+  })
+  return router;
+};
